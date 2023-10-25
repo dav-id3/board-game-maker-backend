2081555113a2fea89257397350736f0326258d8a
dav_id3 <dav_id3@dav-id3s-Laptop.local>
2023-10-26 01:17:57 +0900

    api changed to pub

---
+++ pub/src/app.js
@@ -0,0 +1,77 @@
+const app = require("express")();
+const mysql = require("mysql");
+const server = require("http").createServer(app);
+const connection = mysql.createConnection({
+  host: process.env.MARIADB_HOST || "localhost",
+  port: process.env.MARIADB_PORT || 3306,
+  user: process.env.MARIADB_USER || "user",
+  password: process.env.MARIADB_PASSWORD || "pass",
+  database: process.env.MARIADB_DATABASE || "db",
+});
+const qMessage = "INSERT INTO `messages` (`name`, `text`) VALUES (?, ?)";
+const qCardState =
+  "INSERT INTO `card_states` (`id`, `x`, `y`) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE `x`=?, `y`=?";
+// const qCardState = "UPDATE `card_states` SET `x`=?, `y`=? WHERE `id` = ?";
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.WEB_HOST + ":" + process.env.WEB_PORT,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+const PORT = process.env.API_PORT || 8080;
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(
+    "MYSQL Connected(" +
+      connection.config.host +
+      ":" +
+      connection.config.port +
+      ")."
+  );
+});
+
+io.on("connection", (socket) => {
+  console.log("A client connected.");
+  socket.on("send", (payload) => {
+    console.log(payload);
+    if (payload.topic === "cardState") {
+      connection.query(
+        qCardState,
+        [
+          payload.data.id,
+          payload.data.deltaPosition.x,
+          payload.data.deltaPosition.y,
+          payload.data.deltaPosition.x,
+          payload.data.deltaPosition.y,
+        ],
+        (err, result, fields) => {
+          if (err) throw err;
+        }
+      );
+    } else if (payload.topic === "message") {
+      connection.query(
+        qMessage,
+        [payload.data.name, payload.data.text],
+        (err, result, fields) => {
+          if (err) throw err;
+        }
+      );
+    }
+    socket.broadcast.emit("broadcast", payload);
+  });
+  socket.on("disconnect", () => {
+    console.log("Conenction closed.");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("server listening. Port:" + PORT);
+});
+
+process.on("SIGINT", function () {
+  console.log("Do something useful here.");
+  server.close();
+  process.exit();
+});
